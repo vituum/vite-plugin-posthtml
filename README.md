5b56af4d22a3609e385c6907c3554a5bbbb4d38a
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-21 14:50:25 +0200

    feat: first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<a href="https://npmjs.com/package/@vituum/vite-plugin-posthtml"><img src="https://img.shields.io/npm/v/@vituum/vite-plugin-posthtml.svg" alt="npm package"></a>
+<a href="https://nodejs.org/en/about/releases/"><img src="https://img.shields.io/node/v/@vituum/vite-plugin-posthtml.svg" alt="node compatility"></a>
+
+# ⚡️💡 VitePostHTML
+
+```js
+import posthtml from '@vituum/vite-plugin-posthtml'
+
+export default {
+  plugins: [
+    posthtml({ 
+      plugins: [],
+      options: {}
+    })
+  ]
+}
+```
+
+### Requirements
+
+- [Node.js LTS (16.x)](https://nodejs.org/en/download/)
---
+++ index.js
@@ -0,0 +1,31 @@
+import lodash from 'lodash'
+import posthtmlExtend from 'posthtml-extend'
+import { dirname } from 'path'
+import posthtmlInclude from 'posthtml-include'
+import posthtml from 'posthtml'
+
+const plugin = (pluginOptions = {}) => {
+    pluginOptions = lodash.merge({
+        plugins: []
+    }, pluginOptions)
+
+    return {
+        name: '@vituum/vite-plugin-posthtml',
+        enforce: 'pre',
+        transformIndexHtml: {
+            enforce: 'pre',
+            transform: async(html, { filename }) => {
+                const plugins = [
+                    posthtmlExtend({ encoding: 'utf8', root: dirname(filename) }),
+                    posthtmlInclude({ encoding: 'utf8', root: dirname(filename) })
+                ]
+
+                const result = await posthtml(plugins.concat(...pluginOptions.plugins)).process(html, pluginOptions.options || {})
+
+                return result.html
+            }
+        }
+    }
+}
+
+export default plugin
