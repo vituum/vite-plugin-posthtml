051ed042ad8d358d94836edf1a146cfc7432b9ca
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2025-12-18 15:08:02 +0100

    feat: update deps

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const { name } = getPackageInfo(import.meta.url)
  * @type {import('@vituum/vite-plugin-posthtml/types').PluginUserConfig} pluginOptions
  */
 const defaultOptions = {
-    root: null,
-    extend: {},
-    include: {},
-    plugins: [],
-    options: {}
+  root: null,
+  extend: {},
+  include: {},
+  plugins: [],
+  options: {},
 }
 
 /**
@@ -22,57 +22,58 @@ const defaultOptions = {
  * @returns {import('vite').Plugin}
  */
 const plugin = (pluginOptions = {}) => {
-    pluginOptions = merge(defaultOptions, pluginOptions)
+  pluginOptions = merge(defaultOptions, pluginOptions)
 
-    if (pluginOptions.root) {
-        pluginOptions.root = normalizePath(pluginOptions.root)
-    }
+  if (pluginOptions.root) {
+    pluginOptions.root = normalizePath(pluginOptions.root)
+  }
 
-    return {
-        name,
-        enforce: 'pre',
-        transformIndexHtml: {
-            order: 'pre',
-            handler: async (html, { filename, server }) => {
-                if (filename.replace('.html', '').endsWith('.json') && html.startsWith('{')) {
-                    return html
-                }
-
-                const plugins = []
-
-                if (pluginOptions.extend) {
-                    plugins.push(posthtmlExtend({ root: pluginOptions.root ? pluginOptions.root : dirname(filename), ...pluginOptions.extend }))
-                }
-
-                if (pluginOptions.include) {
-                    plugins.push(posthtmlInclude({ root: pluginOptions.root ? pluginOptions.root : dirname(filename), ...pluginOptions.include }))
-                }
-
-                const render = await new Promise((resolve) => {
-                    const output = {}
-
-                    posthtml(plugins.concat(...pluginOptions.plugins)).process(html, pluginOptions.options).catch(error => {
-                        output.error = error
-                        resolve(output)
-                    }).then(result => {
-                        // @ts-ignore
-                        output.content = result?.html
-                        resolve(output)
-                    })
-                })
-
-                const renderError = pluginError(render.error, server, name)
-
-                if (renderError && server) {
-                    return
-                } else if (renderError) {
-                    return renderError
-                }
-
-                return render.content
-            }
+  return {
+    name,
+    enforce: 'pre',
+    transformIndexHtml: {
+      order: 'pre',
+      handler: async (html, { filename, server }) => {
+        if (filename.replace('.html', '').endsWith('.json') && html.startsWith('{')) {
+          return html
         }
-    }
+
+        const plugins = []
+
+        if (pluginOptions.extend) {
+          plugins.push(posthtmlExtend({ root: pluginOptions.root ? pluginOptions.root : dirname(filename), ...pluginOptions.extend }))
+        }
+
+        if (pluginOptions.include) {
+          plugins.push(posthtmlInclude({ root: pluginOptions.root ? pluginOptions.root : dirname(filename), ...pluginOptions.include }))
+        }
+
+        const render = await new Promise((resolve) => {
+          const output = {}
+
+          posthtml(plugins.concat(...pluginOptions.plugins)).process(html, pluginOptions.options).catch((error) => {
+            output.error = error
+            resolve(output)
+          }).then((result) => {
+            // @ts-ignore
+            output.content = result?.html
+            resolve(output)
+          })
+        })
+
+        const renderError = pluginError(render.error, server, name)
+
+        if (renderError && server) {
+          return
+        }
+        else if (renderError) {
+          return renderError
+        }
+
+        return render.content
+      },
+    },
+  }
 }
 
 export default plugin
